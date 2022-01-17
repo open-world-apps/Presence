0eeb3af0c1e128aaf1627ddae9b81ca8e41b1eff
Brian Blankenship <c0dezer019@zohomail.com>
2022-01-17 01:36:06 -0600

    Initial commit.

---
+++ Presence/Program.cs
@@ -0,0 +1,26 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Presence.Services.Loggers;
+
+namespace Presence;
+
+public class Program
+{
+
+    public static Task Main(string[] args) => new Program().MainAsync();
+
+    private async Task MainAsync()
+    {
+        DiscordSocketClient client = new();
+        EventLogger _logger = new(client);
+        string? token = Environment.GetEnvironmentVariable("TOKEN") ?? 
+                        throw new ArgumentNullException(nameof(token));
+        
+        client.Log += _logger.ClientLogger;
+        await client.LoginAsync(TokenType.Bot, token);
+        await client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+}
+
---
+++ Presence/Services/Loggers/EventLogging.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using Discord;
+using Discord.Commands;
+using Discord.Rest;
+
+namespace Presence.Services.Loggers;
+
+public class EventLogger
+{
+    public EventLogger(BaseDiscordClient client)
+    {
+        if (!File.Exists("/Logs/client_logs.txt"))
+            File.Create("/Logs/client_logs.txt");
+        
+        client.Log += ClientLogger;
+    }
+    public EventLogger(CommandService command)
+    {
+        if (!File.Exists("/Logs/command_logs.txt"))
+            File.Create("/Logs/command_logs.txt");
+        
+        command.Log += CommandLogger;
+    }
+    
+    // ReSharper disable once MemberCanBePrivate.Global
+    [SuppressMessage("ReSharper", "MemberCanBeMadeStatic.Global")]
+    [SuppressMessage("Performance", "CA1822:Mark members as static")]
+    public Task CommandLogger(LogMessage msg)
+
+    {
+        if (msg.Exception is not CommandException cmdException) return Task.CompletedTask;
+        
+        Console.WriteLine($"[Command/{msg.Severity}] {cmdException.Command.Aliases[0]}" +
+                          $" failed to execute in {cmdException.Context.Channel}.");
+        Console.WriteLine(cmdException);
+
+        return Task.CompletedTask;
+    }
+
+    [SuppressMessage("ReSharper", "MemberCanBeMadeStatic.Global")]
+    [SuppressMessage("Performance", "CA1822:Mark members as static")]
+    public Task ClientLogger(LogMessage msg)
+    {
+        
+        return Task.CompletedTask;
+    }
+}
